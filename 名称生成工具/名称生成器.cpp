bdf2a0d94c148687c10735e26ed1f6fbc5879c6f
naci <2641849956@qq.com>
2026-06-22 20:37:45 +0800

    将文件改回原内容

--- 名称生成工具/名称生成器.cpp
+++ 名称生成工具/名称生成器.cpp
@@ -5,8 +5,6 @@ using namespace std;
 #include <ctime>
 #include <Windows.h>
 
-//测试注释，内容1和内容2的合并结果
-
 void cl_wb1();						//处理文本1（获取汉字库）
 void cl_wb2();						//处理文本2（删除由于转成ANSI格式后，错误显示的汉字）
 void cl_wb3();						//处理文本3（提取前面3500字）
